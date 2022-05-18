1847cfd77884402d334707f5dd71d321c0136645
djakish <69299717+djakish@users.noreply.github.com>
2022-05-18 18:12:45 +0600

    prog bar

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
 use scraper::{Html, Selector};
-use std::io::Cursor;
-
+use std::cmp::min;
+use std::io::Write;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-async fn fetch_file(url: String, file_name: String) -> Result<()> {
+async fn fetch_file(u: String, f: String) -> Result<()> {
+    let url = &u[..];
+    let file_name = &f[..];
     let response = reqwest::get(url).await?;
+    let total_size = response
+        .content_length()
+        .ok_or(format!("Failed to get content length from "))?;
+
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .progress_chars("#>-"));
+    pb.set_message(format!("Downloading {}", file_name));
+
     let mut file = std::fs::File::create(file_name)?;
-    let mut content = Cursor::new(response.bytes().await?);
-    std::io::copy(&mut content, &mut file)?;
+    let mut downloaded: u64 = 0;
+    let mut stream = response.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item.or(Err(format!("Error while downloading file")))?;
+        file.write_all(&chunk)
+            .or(Err(format!("Error while writing to file")))?;
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
+    }
+
+    pb.finish_with_message(format!("Downloaded {}", file_name));
+
     Ok(())
 }
 
@@ -20,7 +46,11 @@ async fn get_urls(url: String) -> Result<(String, String)> {
     let video_url = video_item.value().attr("src").unwrap();
 
     let head_selector = Selector::parse("h1").unwrap();
-    let title = fragment.select(&head_selector).next().unwrap().inner_html();
+    let title = match fragment.select(&head_selector).next() {
+        None => "some_title".to_string(),
+        _ => fragment.select(&head_selector).next().unwrap().inner_html(),
+    };
+    //let title = fragment.select(&head_selector).next().unwrap().inner_html();
 
     Ok((video_url.to_string(), title))
 }
@@ -28,9 +58,9 @@ async fn get_urls(url: String) -> Result<(String, String)> {
 #[tokio::main]
 async fn main() {
     let pattern = std::env::args().nth(1).expect("no pattern given");
+
     if pattern.to_string().starts_with("https://vk.com/video") {
         let (url, title) = get_urls(pattern).await.unwrap();
-        println!("{} downloaded", title);
         fetch_file(url, title.to_string() + ".mp4").await.unwrap();
     } else {
         println!("wrong url")
